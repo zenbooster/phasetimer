57e88af67f3d4c7d289935e0ec1b2e19d1f676e0
ZenBooster <zenbooster@gmail.com>
2023-01-19 16:31:37 +0300

    Отказался от Pavlok, т.к. их API сервер работает через раз. Двойной выдох теперь засчитывается только в том случае, если он был произведён после срабатывания таймера. Обратная связь пользователю теперь осуществляется путём подачи звуковых сообщений. Выполнил рефакторинг.

--- main.py
+++ main.py
@@ -19,107 +19,169 @@ import json
 import threading
 import urllib.request
 
-#shared data
-#address = "192.168.1.137:8080"
-address = "192.168.4.40:8080"
-sample_rate = 50
-max_window_dur = 10
-max_window_size = max_window_dur * sample_rate
+import gtts
+import pygame
 
-val = []
-x_data = []
-y_data = []
-z_data = []
-xc_data = []
-yc_data = []
-zc_data = []
-pc_data = []
-peaks = []
-time_data = []
+class TMyApplication:
+    def __init__(self):
+        #self.address = "192.168.1.137:8080"
+        #self.address = "192.168.1.133:8080"
+        self.address = "127.0.0.1:8080"
+        #self.interval_period = 40*60
+        self.interval_period = 20
+        self.alarm_period = 5
 
-x_data_color = "#d32f2f"   # red
-y_data_color = "#7cb342"   # green
-z_data_color = "#0288d1"   # blue
-#da_data_color = "#dada00"   # yellow
-#ee_data_color = "#ee00ee"   # magenta
-pc_data_color = "#000000"   # black
-peaks_color = "#d32f2f"   # red
+        self.val = []
+        self.x_data = []
+        self.y_data = []
+        self.z_data = []
+        self.xc_data = []
+        self.yc_data = []
+        self.zc_data = []
+        self.pc_data = []
+        self.peaks = []
+        self.time_data = []
+        self.alarm_time = 0
 
-background_color = "#fafafa" # white (material)
+        self.pc_data_color = "#000000"   # black
+        self.peaks_color = "#d32f2f"   # red
+        self.background_color = "#fafafa" # white (material)
 
-lock_draw = threading.Lock();
+        self.lock_draw = threading.Lock();
+        
+        self.sound_volume = 1
+        
+    def run(self):
+        msg = 'Привет...'
+        tts = gtts.gTTS(msg, lang = 'ru')
+        tts.save("audio/hi.mp3")
+        #msg = 'Это же сон!'
+        msg = 'Внимание!'
+        tts = gtts.gTTS(msg, lang = 'ru')
+        tts.save("audio/alarm.mp3")
+        msg = 'Принято!'
+        tts = gtts.gTTS(msg, lang = 'ru')
+        tts.save("audio/off.mp3")
 
+        # Подключаемся к Sensor Server для доступа к данным акселерометра смартфона:
+        sensor = Sensor(self, self.address, "android.sensor.accelerometer")
+        sensor.connect() # asynchronous call
+
+        self.alarm_timer = None
+        self.interval_timer = LoopTimer(self.interval_period, self.interval)
+        self.interval_timer.start()
+
+        app = QtWidgets.QApplication(sys.argv)
+
+        pygame.init()
+        pygame.mixer.init()
+
+        pygame.mixer.music.load('audio/hi.mp3')
+        pygame.mixer.music.set_volume(0.5)
+        pygame.mixer.music.play()
+
+        # call on Main thread
+        window = MainWindow(self)
+        window.show()
+
+        res = app.exec_()
+
+        self.interval_timer.cancel()
+        if self.alarm_timer is not None:
+            self.alarm_timer.cancel()
+
+        return res
+
+    def on_alarm_cancel(self):
+        pygame.mixer.music.load('audio/off.mp3')
+        pygame.mixer.music.set_volume(self.sound_volume / 100.0)
+        pygame.mixer.music.play()
+
+        self.sound_volume = 1
+        self.alarm_time = 0
+
+    def interval(self):
+        if self.alarm_timer is None:
+            self.alarm_timer = LoopTimer(self.alarm_period, self.alarm, self.on_alarm_cancel)
+            self.alarm_timer.start()
+
+    def alarm(self):
+        pygame.mixer.music.load('audio/alarm.mp3')
+        pygame.mixer.music.set_volume(self.sound_volume / 100.0)
+        pygame.mixer.music.play()
+
+        if self.sound_volume < 100:
+            self.sound_volume += 2
+        else:
+            self.sound_volume = 100
+
+        if not self.alarm_time:
+            # последний элемент может быть ещё не заполнен:
+            self.alarm_time = self.time_data[-2]
+            print(f'alarm: self.alarm_time = {self.alarm_time}')
 
 class Sensor:
     #constructor
-    def __init__(self,address,sensor_type):
+    def __init__(self, myapp, address, sensor_type):
         self.address = address
         self.sensor_type = sensor_type
         self.length = 0
         self.is_pca_inc = False
         self.last_peak = -1
+        self.sample_rate = 50
+        self.max_window_dur = 10
+        self.max_window_size = self.max_window_dur * self.sample_rate
+        self.myapp = myapp
     
     # called each time when sensor data is recieved
-    def on_message(self,ws, message):
-        #global xc_data, yc_data, zc_data
-        global peaks, alarm_timer, force
-        
-        with lock_draw:
+    def on_message(self, ws, message):
+        with self.myapp.lock_draw:
             values = json.loads(message)['values']
             timestamp = json.loads(message)['timestamp']
 
-            if self.length == max_window_size:
-                val.pop(0)
-                x_data.pop(0)
-                y_data.pop(0)
-                z_data.pop(0)
-                xc_data.pop(0)
-                yc_data.pop(0)
-                zc_data.pop(0)
+            if self.length == self.max_window_size:
+                self.myapp.val.pop(0)
+                self.myapp.x_data.pop(0)
+                self.myapp.y_data.pop(0)
+                self.myapp.z_data.pop(0)
+                self.myapp.xc_data.pop(0)
+                self.myapp.yc_data.pop(0)
+                self.myapp.zc_data.pop(0)
 
                 if self.is_pca_inc:
-                    pc_data.pop(0)
-                    #peaks.pop(0)
+                    self.myapp.pc_data.pop(0)
+                    #self.myapp.peaks.pop(0)
 
-                time_data.pop(0)
+                self.myapp.time_data.pop(0)
                 
-            val.append(values)
-            self.length = len(val)
-            time_data.append(float(timestamp/1000000))
+            self.myapp.val.append(values)
+            self.length = len(self.myapp.val)
+            self.myapp.time_data.append(timestamp / 1000000000.0)
 
             x = values[0]
             y = values[1]
             z = values[2]
 
-            x_data.append(x)
-            y_data.append(y)
-            z_data.append(z)
-            xc_data.append(x)
-            yc_data.append(y)
-            zc_data.append(z)
+            self.myapp.x_data.append(x)
+            self.myapp.y_data.append(y)
+            self.myapp.z_data.append(z)
+            self.myapp.xc_data.append(x)
+            self.myapp.yc_data.append(y)
+            self.myapp.zc_data.append(z)
 
             if self.length > 15:
                 if not self.is_pca_inc:
-                    pc_data.clear()
-                    #peaks.clear()
+                    self.myapp.pc_data.clear()
 
-                #print('HIT.1')
-                
-                #x_data[-1] = x
-                #y_data[-1] = y
-                #z_data[-1] = z
-
-                #print('HIT.2')
                 # Фильтр 2-го порядка для частот выше 2-х Герц.
-                sos = signal.butter(2, 2, 'lp', fs=sample_rate, output='sos')
-                xc_data[-1] = signal.sosfilt(sos, x_data).tolist()[-1]
-                yc_data[-1] = signal.sosfilt(sos, y_data).tolist()[-1]
-                zc_data[-1] = signal.sosfilt(sos, z_data).tolist()[-1]
-                #print('HIT.3')
+                sos = signal.butter(2, 2, 'lp', fs=self.sample_rate, output='sos')
+                self.myapp.xc_data[-1] = signal.sosfilt(sos, self.myapp.x_data).tolist()[-1]
+                self.myapp.yc_data[-1] = signal.sosfilt(sos, self.myapp.y_data).tolist()[-1]
+                self.myapp.zc_data[-1] = signal.sosfilt(sos, self.myapp.z_data).tolist()[-1]
 
                 valc = []
                 for i in range(self.length):
-                    valc.append([xc_data[i], yc_data[i], zc_data[i]])
+                    valc.append([self.myapp.xc_data[i], self.myapp.yc_data[i], self.myapp.zc_data[i]])
 
                 n_components = 1
                 #if self.length >= n_components:
@@ -128,42 +190,39 @@ class Sensor:
 
                 r = pca.fit_transform(av)
                 if self.is_pca_inc:
-                    pc_data[-1] = r[-1][0]
+                    self.myapp.pc_data[-1] = r[-1][0]
                 else:
                     for i in range(self.length):
-                        pc_data.append(r[i][0])
+                        self.myapp.pc_data.append(r[i][0])
                 
-                minPeakHeight = 0.05 #np.std(pc_data)  # this should be tuned
-                pks, peak_props = find_peaks(pc_data, height=minPeakHeight, distance=sample_rate // 2)
-                #print('HIT.4')
-                peaks = [0 for i in range(self.length)]
-                #print(f'HIT.5: {len(peaks)}')
+                minPeakHeight = 0.05 #np.std(self.myapp.pc_data)  # this should be tuned
+                pks, peak_props = find_peaks(self.myapp.pc_data, height=minPeakHeight, distance=self.sample_rate // 2)
+                self.myapp.peaks = [0 for i in range(self.length)]
                 for i in pks:
-                    peaks[i] = 1
+                    self.myapp.peaks[i] = 1
                 
                 if len(pks) >= 2:
                     lp = pks[-1]
-                    #print(f'self.last_peak={self.last_peak}, lp={lp}')
-                    #if self.last_peak != lp:
-                    #    self.last_peak = lp
-                    if alarm_timer is not None:
+                    pp = pks[-2]
+                    if self.myapp.alarm_timer:
+                        print(f'Sensor.on_message: self.myapp.alarm_time = {self.myapp.alarm_time}')
+                        print(f'Sensor.on_message: self.myapp.time_data[lp] = {self.myapp.time_data[lp]}')
+                        print(f'Sensor.on_message: self.myapp.time_data[pp] = {self.myapp.time_data[pp]}')
 
-                        d = lp - pks[-2]
-                        d /= sample_rate
+                    # Если сработал таймер, и двойной выдох был сделан после срабатывания:
+                    if (self.myapp.alarm_timer is not None) and (self.myapp.alarm_time) and (self.myapp.time_data[lp] >= self.myapp.alarm_time) and (self.myapp.time_data[pp] >= self.myapp.alarm_time):
+                        print('HIT.1')
+                        d = lp - pp
+                        d /= self.sample_rate
                         if d <= 1:
-                            print('!! HIT !!')
+                            print('HIT.2')
                             #if alarm_timer is not None:
-                            alarm_timer.cancel()
-                            alarm_timer = None
-                            force = 0
+                            self.myapp.alarm_timer.cancel()
+                            self.myapp.alarm_timer = None
 
-                if self.length == max_window_size and self.last_peak > -1:
-                    self.last_peak -= 1
-
-                #print(f'HIT.6: {len(x_data)}, {len(y_data)}, {len(z_data)}, {len(valc)}, {len(pc_data)}, {len(peaks)}, {len(time_data)}')
             else:
-                pc_data.append(0)
-                peaks.append(0)
+                self.myapp.pc_data.append(0)
+                self.myapp.peaks.append(0)
                 
     def on_error(self,ws, error):
         print("error occurred")
@@ -198,13 +257,15 @@ class Sensor:
 
 class MainWindow(QtWidgets.QMainWindow):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, myapp, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
+        
+        self.myapp = myapp
 
         self.graphWidget = pg.PlotWidget()
         self.setCentralWidget(self.graphWidget)
 
-        self.graphWidget.setBackground(background_color)
+        self.graphWidget.setBackground(myapp.background_color)
 
         self.graphWidget.setTitle("Accelerometer Plot", color="#8d6e63", size="20pt")
         
@@ -214,11 +275,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.graphWidget.setLabel("bottom", "Time (miliseconds)", **styles)
         self.graphWidget.addLegend()
 
-        #self.x_data_line =  self.graphWidget.plot([],[], name="x", pen=pg.mkPen(color=x_data_color))
-        #self.y_data_line =  self.graphWidget.plot([],[], name="y", pen=pg.mkPen(color=y_data_color))
-        #self.z_data_line =  self.graphWidget.plot([],[], name="z", pen=pg.mkPen(color=z_data_color))
-        self.pc_data_line =  self.graphWidget.plot([],[], name="pc", pen=pg.mkPen(color=pc_data_color))
-        self.peaks_line =  self.graphWidget.plot([],[], name="peaks", pen=pg.mkPen(color=peaks_color))
+        self.pc_data_line =  self.graphWidget.plot([],[], name="pc", pen=pg.mkPen(color=myapp.pc_data_color))
+        self.peaks_line =  self.graphWidget.plot([],[], name="peaks", pen=pg.mkPen(color=myapp.peaks_color))
       
         self.timer = QtCore.QTimer()
         self.timer.setInterval(50)
@@ -226,25 +284,23 @@ class MainWindow(QtWidgets.QMainWindow):
         self.timer.start()
 
     def update_plot_data(self):
-        with lock_draw:
+        with self.myapp.lock_draw:
             # limit lists data to 1000 items 
             limit = -1000 
 
             # Update the data.
-            #self.x_data_line.setData(time_data[limit:], x_data[limit:])  
-            #self.y_data_line.setData(time_data[limit:], y_data[limit:])
-            #self.z_data_line.setData(time_data[limit:], z_data[limit:])
-            self.pc_data_line.setData(time_data[limit:], pc_data[limit:])
-            self.peaks_line.setData(time_data[limit:], peaks[limit:])
+            self.pc_data_line.setData(self.myapp.time_data[limit:], self.myapp.pc_data[limit:])
+            self.peaks_line.setData(self.myapp.time_data[limit:], self.myapp.peaks[limit:])
 
 class LoopTimer(threading.Thread):
-    def __init__(self, interval, function, args=None, kwargs=None):
+    def __init__(self, interval, function, on_cancel=None, args=None, kwargs=None):
         super().__init__()
         self.interval = interval
         self.function = function
         self.args = args if args is not None else []
         self.kwargs = kwargs if kwargs is not None else {}
         self.finished = threading.Event()
+        self.on_cancel = on_cancel
 
     def cancel(self):
         """Stop the timer if it hasn't finished yet."""
@@ -254,59 +310,11 @@ class LoopTimer(threading.Thread):
         while not self.finished.is_set():
             if not self.finished.wait(self.interval):
                 self.function(*self.args, **self.kwargs)
+        
+        if self.on_cancel:
+            self.on_cancel()
+                
+myapp = TMyApplication()
+res = myapp.run()
 
-def interval():
-    global alarm_timer
-    if alarm_timer is None:
-        alarm_timer = LoopTimer(5, alarm)
-        alarm_timer.start()
-
-force = 0
-force_list = [
-    ('vibe', 64),
-    ('vibe', 128),
-    ('vibe', 255),
-    ('zap', 10),
-    ('zap', 20),
-    ('zap', 30),
-    ('zap', 40),
-    ('zap', 50)
-]
-def alarm():
-    global force
-    
-    t = force_list[force]
-    mtd = t[0]
-    lvl = t[1]
-    if force < (len(force_list) - 1):
-        force += 1
-
-    contents = urllib.request.urlopen(f'http://127.0.0.1:1337/{mtd}/{lvl}').read()
-
-# Запускаем http сервер для связи с Pavlok:
-p = subprocess.Popen(['node', 'pavlok-srv.js'])
-# Подключаемся к Sensor Server для доступа к данным акселерометра смартфона:
-sensor = Sensor(address=address, sensor_type="android.sensor.accelerometer")
-sensor.connect() # asynchronous call
-
-alarm_timer = None
-interval_timer = LoopTimer(20, interval)
-interval_timer.start()
-
-app = QtWidgets.QApplication(sys.argv)
-
-print('HIT.1')
-# call on Main thread
-window = MainWindow()
-window.show()
-
-res = app.exec_()
-
-print('HIT.2')
-interval_timer.cancel()
-if alarm_timer is not None:
-    alarm_timer.cancel()
-
-p.terminate()
-print('HIT.3')
 sys.exit(res)
---
+++ test-sensor.sh
@@ -0,0 +1,2 @@
+# https://github.com/vi/websocat/releases
+~/Downloads/websocat.x86_64-unknown-linux-musl ws://127.0.0.1:8080/sensor/connect?type=android.sensor.accelerometer
